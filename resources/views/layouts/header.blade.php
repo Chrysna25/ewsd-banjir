e842fc517370a6f1191db67afcd60fdcc047c4a0
dededwiwiraputra <dededwiwiraputra17@gmail.com>
2023-11-20 14:51:39 +0700

    Lagi-lagi Vercel Setup

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -24,4 +24,6 @@
   <!-- Nepcha Analytics (nepcha.com) -->
   <!-- Nepcha is a easy-to-use web analytics. No cookies and fully compliant with GDPR, CCPA and PECR. -->
   <script defer data-site="YOUR_DOMAIN_HERE" src="https://api.nepcha.com/js/nepcha-analytics.js"></script>
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
